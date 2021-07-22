cb0284d24a0987f5f0ba6cb6d7f579fbd4e6aaf9
Yujee Song <rswfaf@gmail.com>
2021-07-22 18:46:45 +0900

    Added airport search API

--- server/urls.py
+++ server/urls.py
@@ -1,15 +1,7 @@
-from django.urls import path, include
-from rest_framework.generics import GenericAPIView
-from server.views import AirportViewSet, AirportGenericAPIView
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from server.views import AirportAPIView
 
-router = DefaultRouter()
-router.register("airport", AirportViewSet, basename="airport")
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path(
-        "generic",
-        GenericAPIView.as_view(),
-    ),
+    path("airport/<str:name>/", AirportAPIView.as_view()),
 ]
