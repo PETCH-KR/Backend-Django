cdec9a640cc776b5614ca3a7dd10e12aedb3969c
ziho <ziho@kakao.com>
2021-07-23 11:09:42 +0900

    Update Django Secret key, MongoDB host

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rjdhii^8xap9^&1j6q0t&iegh)&9v%g3hb_j@8e$-*y^f=f4ju"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,7 @@ DATABASES = {
         "NAME": "Petch",
         "ENFORCE_SCHEMA": False,
         "CLIENT": {
-            "host": "mongodb+srv://capstone:capstone@cluster0.yzq7p.mongodb.net/Petch?retryWrites=true&w=majority"
+            "host": os.getenv("MONGO_HOST")
         },
     }
 }
