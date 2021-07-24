3d40763ea064740bee82b99426cc2e91efc01c66
ziho <ziho@kakao.com>
2021-07-24 16:31:15 +0900

    Add user url

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from server.views import AirportAPIView
+from server.views import AirportAPIView, UserView
 
 
 urlpatterns = [
+    path("", UserView.as_view()),
     path("airport/<str:name>/", AirportAPIView.as_view()),
 ]
