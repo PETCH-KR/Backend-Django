608817fa1e718681b642f807ee58cf5e20cf0227
ziho <ziho@kakao.com>
2021-07-25 17:19:13 +0900

    Update user serializer
    provider에 따라 비밀번호가 필요한 경우, 필요 없는 경우 나눔.

--- server/serializers/user.py
+++ server/serializers/user.py
@@ -3,8 +3,7 @@ from server.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
@@ -13,11 +12,17 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "phone",
             "passport",
+            "provider",
         ]
 
     def create(self, validated_data):
-        password = validated_data.get("password")
-        user = super().create(validated_data)
-        user.set_password(password)
-        user.save()
+        if validated_data.get("password"):
+            password = validated_data.get("password")
+            user = super().create(validated_data)
+            user.set_password(password)
+            user.save()
+        # 비밀번호 필요 없는 경우, SNS 로그인
+        else:
+            user = super().create(validated_data)
+            user.save()
         return user
