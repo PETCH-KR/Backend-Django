72c663357ca7ceaa8a98cf4f1da5eea1b6b1767d
ziho <ziho@kakao.com>
2021-07-25 17:18:07 +0900

    Update user model's fields

--- server/models/user.py
+++ server/models/user.py
@@ -4,8 +4,13 @@ from django.db import models
 
 class User(AbstractBaseUser):
 
-    email = models.CharField(max_length=100, unique=True)
-    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=128, null=True)
+    phone = models.CharField(max_length=15, null=True)
     passport = models.BooleanField(default=False)
+    # provider = DEFAULT | APPLE | GOOGLE | KAKAO
+    provider = models.CharField(max_length=20, default="DEFAULT")
+    token = models.CharField(max_length=255, null=True)
 
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
