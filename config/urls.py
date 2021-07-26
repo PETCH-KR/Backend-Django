da4615f010878c7d32b757bde0853dd0414727b3
ziho <ziho@kakao.com>
2021-07-26 20:38:53 +0900

    Add swagger docs page

--- config/urls.py
+++ config/urls.py
@@ -15,5 +15,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("server.urls"))]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Petch API 문서",
+        default_version="v1",
+        description="Petch API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="swagger test", email="cau.capstone10@gmail.com"),
+        license=openapi.License(name="swagger license"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path(
+        "swagger<str:format>",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
+    path("api/", include("server.urls")),  # http://localhost:8000/api/
+]
