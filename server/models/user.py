a3b0bb98732b244361d7fa463321fae034ed73e6
ziho <ziho@kakao.com>
2021-07-26 20:39:59 +0900

    Add user api detail on swagger docs

--- server/models/user.py
+++ server/models/user.py
@@ -4,13 +4,28 @@ from django.db import models
 
 class User(AbstractBaseUser):
 
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=128, null=True)
-    phone = models.CharField(max_length=15, null=True)
-    passport = models.BooleanField(default=False)
+    email = models.EmailField(
+        max_length=100, unique=True, verbose_name="Email", help_text="이메일"
+    )
+    password = models.CharField(
+        max_length=128, null=True, verbose_name="password", help_text="비밀번호"
+    )
+    phone = models.CharField(
+        max_length=15, null=True, verbose_name="phone", help_text="전화번호"
+    )
+    passport = models.BooleanField(
+        default=False, verbose_name="passport", help_text="여권"
+    )
     # provider = DEFAULT | APPLE | GOOGLE | KAKAO
-    provider = models.CharField(max_length=20, default="DEFAULT")
-    token = models.CharField(max_length=255, null=True)
+    provider = models.CharField(
+        max_length=20,
+        default="DEFAULT",
+        verbose_name="provider",
+        help_text="DEFAULT | APPLE | GOOGLE | KAKAO",
+    )
+    token = models.CharField(
+        max_length=255, null=True, verbose_name="token", help_text="Refresh Token"
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- server/views/user.py
+++ server/views/user.py
@@ -2,9 +2,41 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from server.serializers import UserSerializer
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 class SignupView(APIView):
+    success_field = openapi.Schema(
+        "success", description="성공 여부", type=openapi.TYPE_BOOLEAN
+    )
+    message_field = openapi.Schema(
+        "detail", description="메세지", type=openapi.TYPE_STRING
+    )
+    data_field = openapi.Schema("data", description="회원 정보", type=openapi.TYPE_OBJECT)
+    success_resp = openapi.Schema(
+        "response",
+        type=openapi.TYPE_OBJECT,
+        properties={
+            "success": success_field,
+            "message": message_field,
+            "data": data_field,
+        },
+    )
+    http_method_names = ["post"]
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="이메일"),
+                "password": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="비밀번호"
+                ),
+            },
+        ),
+        responses={200: success_resp},
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,7 +46,6 @@ class SignupView(APIView):
                 "message": "성공적으로 회원가입이 완료되었습니다.",
                 "data": {"user": UserSerializer(new_user).data},
             }
-
             return Response(response_object)
         else:
             response_object = {
