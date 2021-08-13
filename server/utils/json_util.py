dc10e1890400515bbc6f62eb9aed4e49a8426ccb
ziho <ziho@kakao.com>
2021-08-13 20:26:02 +0900

    Add json_util

---
+++ server/utils/json_util.py
@@ -0,0 +1,13 @@
+import json
+from bson import ObjectId
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+def jsonify(data):
+    return json.loads(json.dumps(data, cls=JSONEncoder))
