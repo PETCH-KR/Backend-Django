cb2bc027d4b46dc0fc8db22fdc23377ac1cd2ae9
Yujee Song <rswfaf@gmail.com>
2021-09-05 23:34:48 +0900

    Add 'chat' in installed apps

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_extensions",
     "corsheaders",
+    "channels",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
