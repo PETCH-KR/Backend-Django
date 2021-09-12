e804ff3b0bf052573016b10ad171d849965e789d
ziho <ziho@kakao.com>
2021-09-12 16:37:12 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,7 @@ python manage.py migrate
 python manage.py collectstatic
 
 gunicorn config.wsgi -b 0.0.0.0:8000
+
+
+daphne -e ssl:8000:privateKey=/etc/letsencrypt/live/catchi-nichi.tk/privkey.pem:certKey=/etc/letsencrypt/live/catchi-nichi.tk/fullchain.pem: config.asgi:application
+
