d89f5c104ffe2e9803a147a68aaf4a9a8c891337
JardinDelSol <rswfaf@gmail.com>
2021-09-12 17:24:16 +0900

    Edit for Daphne server

--- config/asgi.py
+++ config/asgi.py
@@ -11,9 +11,11 @@ import os
 
 # from django.core.asgi import get_asgi_application
 from channels.routing import get_default_application
+import django 
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # application = get_asgi_application()
+django.setup()
 application = get_default_application()
--- config/routing.py
+++ config/routing.py
@@ -1,9 +1,11 @@
+from config.settings import ASGI_APPLICATION
 from .asgi import *
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import chat.routing
 
+ASGI_APPLICATION="config.routing.application"
 
 application = ProtocolTypeRouter(
     {"websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))}
